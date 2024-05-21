719050b0eab7fa211ff4b7971aae58b6adb90235
Marcomussa <marcomussa567@gmail.com>
2024-05-21 18:11:01 -0300

    k

--- clients/shopifyClient.js
+++ clients/shopifyClient.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-
 const SHOPIFY_STORE_URL = process.env.SHOPIFY_STORE_URL
 const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY 
 const SHOPIFY_API_SECRET_KEY = process.env.SHOPIFY_API_SECRET_KEY 
@@ -7,13 +6,14 @@ const SHOPIFY_ACCESS_TOKEN = process.env.SHOPIFY_ACCESS_TOKEN
 
 const headers = {
     'Content-Type': 'application/json',
-    [`${SHOPIFY_API_KEY}`]: `${SHOPIFY_API_SECRET_KEY}`,
-    'X-Shopify-Access-Token': SHOPIFY_ACCESS_TOKEN
+    '6e169eac360293c61f1ab3b856359293': `${SHOPIFY_API_SECRET_KEY}`,
+    'X-Shopify-Access-Token': `${SHOPIFY_ACCESS_TOKEN}`,
+    'Host': `${SHOPIFY_STORE_URL}`
 };
 
 exports.createProduct = async (productData) => {
   const response = await axios.post(SHOPIFY_STORE_URL, productData, { headers });
-  return response;
+  return response.data;
 };
 
 exports.updateProduct = async (id, productData) => {
