79e86105b9eb855594f2a92f4076d46af8c50e83
Marcomussa <marcomussa567@gmail.com>
2024-07-31 11:11:52 -0300

    k

--- routes/orders.js
+++ routes/orders.js
@@ -4,17 +4,37 @@ const app = express()
 const router = express.Router()
 const bodyParser = require('body-parser')
 const crypto = require('crypto')
+const SHOPIFY_SECRET = "6b62086837a63bb49b3e44e45b7eac4aa1db77e9be0e88c7f6f7a03632b88412"
 
-router.post('/new', (req, res) => {
-    try {
-        const order = req.body
-        console.log('Orden creada: ', order)
-    
-        res.status(200).send('Webhook recibido exitosamente')
-      } catch (error) {
-        console.error('Error procesando el webhook: ', error)
-        res.status(401).send('Webhook verification failed x')
-      }
+// Middleware para parsear el cuerpo de la solicitud y almacenar el cuerpo sin procesar
+app.use(bodyParser.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  }
+}));
+
+const verifyShopifyWebhook = (req, res, next) => {
+  const hmacHeader = req.get('X-Shopify-Hmac-Sha256');
+  const body = req.rawBody;
+  const hash = crypto.createHmac('sha256', SHOPIFY_SECRET)
+                     .update(body, 'utf8', 'hex')
+                     .digest('base64')
+
+  if (hash === hmacHeader) {
+    next();
+  } else {
+    res.status(401).send('Webhook verification failed')
+  }
+}
+
+router.post('/new', verifyShopifyWebhook, (req, res) => {
+  const orderData = req.body
+
+  console.log('Nueva orden recibida:', orderData)
+
+  // Logica
+
+  res.status(200).send('Webhook recibido correctamente')
 })
 
 module.exports = router
