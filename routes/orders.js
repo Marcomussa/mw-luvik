b1a0e3e3b3319edbc1ddcd77f40bc1fac9c71703
Marcomussa <marcomussa567@gmail.com>
2024-07-31 13:21:32 -0300

    j

--- routes/orders.js
+++ routes/orders.js
@@ -4,19 +4,18 @@ const app = express()
 const router = express.Router()
 const bodyParser = require('body-parser')
 const crypto = require('crypto')
+const getRawBody = require('raw-body')
 const SHOPIFY_SECRET = process.env.WEBHOOK_SECRET
 
-app.use(bodyParser.json({
-  verify: (req, res, buf) => {
-    console.log(buf)
-    req.rawBody = buf
-  }
-}))
+// app.use(bodyParser.json({
+//   verify: (req, res, buf) => {
+//     req.rawBody = buf
+//   }
+// }))
 
 const verifyShopifyWebhook = (req, res, next) => {
   const hmacHeader = req.get('X-Shopify-Hmac-Sha256');
   const body = req.rawBody
-  console.log(body)
 
   const hash = crypto.createHmac('sha256', SHOPIFY_SECRET).update(body, 'utf8', 'hex').digest('base64')
   if (hash === hmacHeader) {
@@ -26,7 +25,24 @@ const verifyShopifyWebhook = (req, res, next) => {
   }
 }
 
-router.post('/new', verifyShopifyWebhook, (req, res) => {
+router.post('/new', async (req, res) => {
+  const hmac = req.get('X-Shopify-Hmac-Sha256')
+  const body = await getRawBody(req)
+  const hash = crypto.createHmac('sha256', SHOPIFY_SECRET).update(body, 'utf8', 'hex').digest('base64')
+
+  if (hash === hmac) {
+    console.log('Nueva orden recibida:', body)
+    res.sendStatus(200)
+  } else {
+    console.log('error')
+    res.sendStatus(403)
+  }
+  //! Logic
+
+  res.status(200).send('Webhook recibido correctamente')
+})
+
+router.post('/new-test', (req, res) => {
   const orderData = req.body
   console.log('Nueva orden recibida:', orderData)
 
