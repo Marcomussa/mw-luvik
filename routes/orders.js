6a68aaf1541edac478057e885d440444e162312f
Marcomussa <marcomussa567@gmail.com>
2024-08-02 11:58:42 -0300

    k

--- routes/orders.js
+++ routes/orders.js
@@ -6,22 +6,10 @@ const bodyParser = require('body-parser')
 const crypto = require('crypto')
 const SHOPIFY_SECRET = process.env.WEBHOOK_SECRET
 
-app.use(bodyParser.raw({ type: 'application/json' }));
-
 router.post('/new', async (req, res) => {
-  const hmac = req.get('X-Shopify-Hmac-Sha256')
   const body = req.body
-  const hash = crypto.createHmac('sha256', SHOPIFY_SECRET).update(body).digest('base64')
-
-  if (hash === hmac) {
-    console.log('Nueva orden recibida:', body)
-    res.sendStatus(200)
-  } else {
-    console.log('error')
-    res.sendStatus(403)
-  }
   //! Logic
-
+  console.log(body)
   res.status(200).send('Webhook recibido correctamente')
 })
 
