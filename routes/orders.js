adbe6bd053c57639e855bab5f001435e6eed7d0e
Marco <marcomussa567@gmail.com>
2024-08-02 16:28:12 -0300

    k

--- routes/orders.js
+++ routes/orders.js
@@ -6,15 +6,17 @@ const bodyParser = require('body-parser')
 const crypto = require('crypto')
 const SHOPIFY_SECRET = process.env.WEBHOOK_SECRET
 
-app.use(bodyParser.raw({ type: 'application/json' }));
+app.use(express.raw({ type: 'application/json' }));
 
 function validateSignature(req, res, next) {
   const receivedSignature = req.headers['x-shopify-hmac-sha256'];
   if (!receivedSignature) {
-    return res.status(400).send('No se encontró la firma en los encabezados');
+    console.log("No se encontró la firma en los encabezados")
+    return res.status(400)
   }
 
-  const rawBody = Buffer.isBuffer(req.body) ? req.body : Buffer.from(req.body);
+  const rawBody = Buffer.isBuffer(req.body) ? req.body : Buffer.from(req.body)
+  console.log(typeof rawBody)
   const generatedSignature = crypto
       .createHmac('sha256', SHOPIFY_SECRET)
       .update(rawBody)
@@ -31,7 +33,7 @@ function validateSignature(req, res, next) {
     next();
   } else {
     console.log('Firma inválida');
-    res.status(401).send('Firma no válida');
+    return res.status(401)
   }
 }
 
