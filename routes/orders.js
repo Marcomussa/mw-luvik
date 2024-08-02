5c28817462e52c6eeece4e0f05b289f32bf10757
Marcomussa <marcomussa567@gmail.com>
2024-08-02 11:17:30 -0300

    k

--- routes/orders.js
+++ routes/orders.js
@@ -7,11 +7,11 @@ const crypto = require('crypto')
 const getRawBody = require('raw-body')
 const SHOPIFY_SECRET = process.env.WEBHOOK_SECRET
 
-// app.use(bodyParser.json({
-//   verify: (req, res, buf) => {
-//     req.rawBody = buf
-//   }
-// }))
+app.use(bodyParser.json({
+   verify: (req, res, buf) => {
+    req.rawBody = buf
+   }
+}))
 
 const verifyShopifyWebhook = (req, res, next) => {
   const hmacHeader = req.get('X-Shopify-Hmac-Sha256');
@@ -27,8 +27,9 @@ const verifyShopifyWebhook = (req, res, next) => {
 
 router.post('/new', async (req, res) => {
   const hmac = req.get('X-Shopify-Hmac-Sha256')
-  const body = req.body
-  const hash = crypto.createHmac('sha256', SHOPIFY_SECRET).update(body, 'utf8', 'hex').digest('base64')
+  const body = req.rawBody
+  console.log(body)
+  const hash = crypto.createHmac('sha256', process.env.SHOPIFY_API_SECRET_KEY).update(body, 'utf8', 'hex').digest('base64')
 
   if (hash === hmac) {
     console.log('Nueva orden recibida:', body)
